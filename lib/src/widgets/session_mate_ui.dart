b46bfddd6d99b58d37bab064ddb6ed81b1cfb7d7
Dane Mackier <dane.mackier@outlook.com>
2023-08-14 09:20:47 +0200

    Adds the SessionMate widget that swaps between recording and replaying

--- lib/src/widgets/session_mate_ui.dart
+++ lib/src/widgets/session_mate_ui.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:session_mate/session_mate.dart';
 
 class SessionMate extends StatelessWidget {
-  const SessionMate({Key? key}) : super(key: key);
+  final Widget child;
+  const SessionMate({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final showInteractionRecorder = kReleaseMode || kProfileMode;
+
+    return showInteractionRecorder
+        ? InteractionRecorder(child: child)
+        : DriverUI(child: child);
   }
 }
