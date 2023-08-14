564300139915abde91a196ec25d0aa6a60077bbe
Fernando Ferrara <ferrarafer@gmail.com>
2023-08-14 10:21:02 -0300

    feat: add SessionService

--- lib/src/app/locator_setup.dart
+++ lib/src/app/locator_setup.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:session_mate/src/services/driver_communication_service.dart';
+import 'package:session_mate/src/services/session_service.dart';
 
 final locator = GetIt.asNewInstance();
 
 Future<void> setupLocator() async {
   locator.registerLazySingleton(() => DriverCommunicationService());
+  locator.registerLazySingleton(() => SessionService());
 }
