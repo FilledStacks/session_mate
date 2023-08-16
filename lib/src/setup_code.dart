c3e42ebbfc43180fbb01f409be7899b8b7e5b967
Dane Mackier <dane.mackier@outlook.com>
2023-08-16 09:37:21 +0200

    Fixes final hive issue

--- lib/src/setup_code.dart
+++ lib/src/setup_code.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/widgets.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:session_mate/session_mate.dart';
 import 'package:session_mate/src/services/driver_communication_service.dart';
@@ -8,6 +9,13 @@ import 'package:session_mate/src/services/hive_storage_service.dart';
 import 'app/locator_setup.dart';
 
 Future<void> setupSessionMate() async {
+  enableFlutterDriverExtension(
+    handler: (message) async {
+      final driverCommunicationService = locator<DriverCommunicationService>();
+      return driverCommunicationService.waitForInteractions();
+    },
+  );
+
   HttpOverrides.global = SessionMateHttpOverrides();
 
   // Here we can setup our locator since we are going to be using services as well
@@ -17,11 +25,4 @@ Future<void> setupSessionMate() async {
   final hiveService = locator<HiveStorageService>();
   print('Sessions: ${hiveService.getSessions().length}');
   print('================== END SESSION DETAILS ==================');
-
-  enableFlutterDriverExtension(
-    handler: (message) async {
-      final driverCommunicationService = locator<DriverCommunicationService>();
-      return driverCommunicationService.waitForInteractions();
-    },
-  );
 }
