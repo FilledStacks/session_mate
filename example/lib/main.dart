c6b00c426aa7e293a19fbcbddb8edbb4ff4de5e8
Fernando Ferrara <ferrarafer@gmail.com>
2023-08-17 23:25:55 -0300

    feat: add example app

---
+++ example/lib/main.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:session_mate/session_mate.dart';
+
+const String baseUrl = 'www.googleapis.com';
+const String booksEndpoint = '/books/v1/volumes';
+
+void main() async {
+  await setupSessionMate();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SessionMate(
+        child: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> _topics = [
+    'http',
+    'dart',
+    'flutter',
+    'development',
+    'patterns',
+  ];
+
+  int _counter = 0;
+  String _topic = 'http';
+  String _desc = '';
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Future<void> _requestBooks() async {
+    _topic = _topics[_counter % _topics.length];
+    final url = Uri.https(baseUrl, booksEndpoint, {'q': '{$_topic}'});
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+      final itemCount = jsonResponse['totalItems'];
+      _desc = 'Number of books about $_topic: $itemCount.';
+    } else {
+      _desc = 'Request failed with status: ${response.statusCode}.';
+    }
+
+    _incrementCounter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_desc),
+            const SizedBox(height: 50),
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _requestBooks,
+        tooltip: 'Request Books',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/fermento/.asdf/installs/flutter/3.10.6-stable"
-export "FLUTTER_APPLICATION_PATH=/Users/fermento/Development/Filledstacks/session_mate/session_mate/example"
+export "FLUTTER_APPLICATION_PATH=/Users/fermento/Development/Filledstacks/session_mate_org/session_mate/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/fermento/Development/Filledstacks/session_mate/session_mate/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.1.0"
 export "FLUTTER_BUILD_NUMBER=0.1.0"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/fermento/Development/Filledstacks/session_mate/session_mate/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
