70373bce6a2e1c10b5fd156a71cbe80ce5c11df0
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-08 00:11:14 -0300

    feat: add dart environment options
    - Added kRecordUserInteractions configuration option
    - Added kForceDriverUI configuration option

--- lib/src/package_constants.dart
+++ lib/src/package_constants.dart
@@ -1,9 +1,5 @@
-const bool _recordSession = bool.fromEnvironment(
-  'RECORD_SESSION',
-  defaultValue: false,
-);
-
-const bool kRecordUserInteractions = _recordSession;
+const bool kRecordUserInteractions = bool.fromEnvironment('RECORD_SESSION');
+const bool kForceDriverUI = bool.fromEnvironment('FORCE_DRIVER_UI');
 
 const double kHorizontalPadding = 10;
 const double kVerticalPadding = 30;
