9f59ff7b119e96c146eaf4c5c8221c079fe2ecd3
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-14 19:25:21 -0300

    feat: add onRestart callback on SessionMate

--- lib/src/widgets/driver_ui/driver_ui.dart
+++ lib/src/widgets/driver_ui/driver_ui.dart
@@ -9,11 +9,8 @@ import 'package:stacked/stacked.dart';
 
 class DriverUI extends StackedView<DriverUIViewModel> {
   final Widget child;
-
-  const DriverUI({
-    Key? key,
-    required this.child,
-  }) : super(key: key);
+  final VoidCallback? onRestart;
+  const DriverUI({super.key, required this.child, this.onRestart});
 
   @override
   Widget builder(
@@ -53,9 +50,10 @@ class DriverUI extends StackedView<DriverUIViewModel> {
             if (viewModel.showSessionList) const SessionList(),
             // const SessionActions(),
             Positioned(
-                bottom: MediaQuery.of(context).size.height * .025,
-                left: MediaQuery.of(context).size.width * .05,
-                child: const DriverBar()),
+              bottom: MediaQuery.of(context).size.height * .025,
+              left: MediaQuery.of(context).size.width * .05,
+              child: DriverBar(onRestart: onRestart),
+            ),
           ],
         ],
       ),
