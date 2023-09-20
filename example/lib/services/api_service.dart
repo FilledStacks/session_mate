cd973af5dd66d99bdbeb0ac2c7a6f5a0c2bd5468
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-20 01:33:37 -0300

    fix: update example app
    - Throw exception for status code 429
    - Show the index instead of bookId on BookCard
    - Use environment variable to use fake data

--- example/lib/services/api_service.dart
+++ example/lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:bookshelf/app/app.logger.dart';
 import 'package:bookshelf/models/book.dart';
 import 'package:http/http.dart' as http;
 
@@ -7,11 +8,23 @@ class ApiService {
   static const String baseUrl = 'www.googleapis.com';
   static const String books = '/books/v1/volumes';
 
-  Future<Iterable<Book>> getBooks({String genreType = 'computers'}) async {
-    final url = Uri.https(baseUrl, books, {'q': 'subject:$genreType'});
-    final response = await http.get(url);
-    final Iterable data = jsonDecode(response.body)['items'];
+  final _logger = getLogger('ApiService');
 
-    return data.map((e) => Book.fromJson(e['volumeInfo']));
+  Future<Iterable<Book>> getBooks({String genreType = 'computers'}) async {
+    try {
+      final url = Uri.https(baseUrl, books, {'q': 'subject:$genreType'});
+      final response = await http.get(url);
+
+      if (response.statusCode == 429) {
+        throw Exception(jsonDecode(response.body)['error']['message']);
+      }
+
+      final Iterable data = jsonDecode(response.body)['items'];
+
+      return data.map((e) => Book.fromJson(e['volumeInfo']));
+    } catch (e) {
+      _logger.e(e.toString());
+      rethrow;
+    }
   }
 }
--- example/lib/ui/views/books_list/books_list_viewmodel.dart
+++ example/lib/ui/views/books_list/books_list_viewmodel.dart
@@ -3,12 +3,15 @@ import 'package:bookshelf/app/app.logger.dart';
 import 'package:bookshelf/app/app.router.dart';
 import 'package:bookshelf/fake_data/fake_book.dart';
 import 'package:bookshelf/models/book.dart';
+import 'package:bookshelf/services/api_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+const kUseFakeData = bool.fromEnvironment('USE_FAKE_DATA');
+
 class BooksListViewModel extends FutureViewModel<List<Book>> {
   final _logger = getLogger('BooksListViewModel');
-  // final _api = locator<ApiService>();
+  final _api = locator<ApiService>();
   final _navigationService = locator<NavigationService>();
 
   @override
@@ -17,9 +20,13 @@ class BooksListViewModel extends FutureViewModel<List<Book>> {
   Future<List<Book>> getBooks({String genreType = 'computers'}) async {
     _logger.i('');
 
-    await Future.delayed(const Duration(seconds: 1));
+    if (kUseFakeData) {
+      await Future.delayed(const Duration(seconds: 1));
 
-    return FakeBook().generateFakeList(length: 100);
+      return FakeBook().generateFakeList(length: 100);
+    }
+
+    return (await _api.getBooks()).toList();
   }
 
   void onTap(Book book) {
