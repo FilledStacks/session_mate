2c7feac06c80a11f588d8377638b4dc0c6e78571
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-20 02:39:52 -0300

    fix: avoid record a session on replay mode

--- lib/src/session_mate_utils.dart
+++ lib/src/session_mate_utils.dart
@@ -1,4 +1,5 @@
 import 'package:session_mate/src/app/locator_setup.dart';
+import 'package:session_mate/src/package_constants.dart';
 import 'package:session_mate/src/services/hive_service.dart';
 import 'package:session_mate/src/services/session_service.dart';
 import 'package:session_mate_core/session_mate_core.dart';
@@ -7,6 +8,11 @@ class SessionMateUtils {
   static void saveSession({SessionPriority priority = SessionPriority.high}) {
     final sessionService = locator<SessionService>();
     final localStorageService = locator<HiveService>();
+
+    if (!kRecordUserInteractions) {
+      print('🎥 Session not recorded, you are not on a recording session.');
+      return;
+    }
 
     try {
       localStorageService.saveSession(sessionService.captureSession(
