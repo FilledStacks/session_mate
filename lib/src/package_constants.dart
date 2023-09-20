3575f4682e512d7eb55d1a5643de05a1a40bb447
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-20 02:39:23 -0300

    feat: add more environment variables

--- lib/src/package_constants.dart
+++ lib/src/package_constants.dart
@@ -3,6 +3,8 @@ import 'package:flutter/foundation.dart';
 const bool kRecordUserInteractions =
     !kDebugMode || bool.fromEnvironment('RECORD_SESSION');
 const bool kForceDriverUI = bool.fromEnvironment('FORCE_DRIVER_UI');
+const bool kUseFakeData = bool.fromEnvironment('USE_FAKE_DATA');
+const bool kDisableDataMasking = bool.fromEnvironment('DISABLE_DATA_MASKING');
 
 const double kHorizontalPadding = 10;
 const double kVerticalPadding = 30;
