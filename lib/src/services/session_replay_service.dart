21da9e7f5a5246d5ec8b9c25c3168ab7547175a4
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-26 19:43:10 -0300

    refactor: improve SessionReplayService
    - Added handleMockRequest method to handle the request from the local server and send it back to the guest app
    - Removed unnecessary code

--- lib/src/services/session_replay_service.dart
+++ lib/src/services/session_replay_service.dart
@@ -1,52 +1,62 @@
+import 'dart:io';
+
 import 'package:session_mate/session_mate.dart';
 import 'package:session_mate/src/helpers/crypto_helper.dart';
+import 'package:session_mate/src/helpers/response_filter_helper.dart';
 import 'package:session_mate/src/package_constants.dart';
 import 'package:session_mate_core/session_mate_core.dart';
 
 class SessionReplayService {
-  final Map<String, String> _requests = {};
+  /// Stores the hash of each request
+  final Map<String, String> _requestsHashes = {};
 
-  final Map<String, NetworkEvent> _cache = {};
+  /// Stores the responses that were masked under session recording
+  final Map<String, ResponseEvent> _maskedResponses = {};
 
   NetworkEvent? _currentEvent;
 
-  bool get _isCurrentEventARequest =>
-      _currentEvent != null && _currentEvent is RequestEvent;
-
+  // TODO: use only RequestEvent here
   void handleEvent(NetworkEvent event) {
     _currentEvent = event;
     if (event is RequestEvent) {
-      _requests[event.uid] = hashEvent(event);
+      _requestsHashes[event.uid] = hashEvent(event);
     }
   }
 
   void populateCache(List<NetworkEvent> events) {
     print(
-      'SessionService - populate ${events.length} network events into cache',
+      'SessionReplayService - populate ${events.length} masked responses into cache',
     );
 
     for (var e in events) {
-      _cache[(e as ResponseEvent).uid] = e;
+      _maskedResponses[(e as ResponseEvent).uid] = e;
+    }
+  }
+
+  Future<void> handleMockRequest(HttpRequest request) async {
+    try {
+      // NOTE: what if we hash the request directly here to avoid using uid?
+
+      final response = _maskedResponses[
+          _requestsHashes[(_currentEvent as RequestEvent).uid]]!;
+
+      request.response
+        ..headers.host = response.headers['host']
+        ..write(response.body)
+        ..close();
+    } catch (e) {
+      print('$e');
     }
   }
 
   Future<List<int>> getSanitizedData(List<int> data, {String? uid}) async {
-    if (kRecordUserInteractions) return data;
+    if (kRecordUserInteractions || uid == null) return data;
 
-    while (_isCurrentEventARequest) {
-      await Future.delayed(Duration(microseconds: 100));
-    }
-
-    if (uid == null) return data;
-
-    final response = _cache[_requests[uid]] as ResponseEvent?;
+    final response = _maskedResponses[_requestsHashes[uid]];
 
     if (response == null) return data;
 
-    if (response.headers['content-type']!.contains('image')) {
-      // return globalPlaceHolder;
-      return gPlaceHolder;
-    }
+    if (hasImageContentType(response)) return gPlaceHolder;
 
     return response.body ?? data;
   }
