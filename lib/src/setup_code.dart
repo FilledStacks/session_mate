b17f1ae6e44eb59cae598e4d2f0fa9129e09a7c0
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-26 19:48:37 -0300

    feat: start an HttpServer on replay mode

--- lib/src/setup_code.dart
+++ lib/src/setup_code.dart
@@ -6,7 +6,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:session_mate/session_mate.dart';
 import 'package:session_mate/src/package_constants.dart';
+import 'package:session_mate/src/services/configuration_service.dart';
 import 'package:session_mate/src/services/driver_communication_service.dart';
+import 'package:session_mate/src/services/session_replay_service.dart';
 
 import 'app/locator_setup.dart';
 
@@ -37,4 +39,17 @@ Future<void> setupSessionMate() async {
   );
   // globalPlaceHolder = base64.encode(bytes.buffer.asUint8List());
   globalPlaceHolder = bytes.buffer.asUint8List();
+
+  if (!kRecordUserInteractions) {
+    HttpServer.bind(InternetAddress.loopbackIPv4, 0).then((HttpServer server) {
+      print('📻 listening on ${server.address}, port ${server.port}');
+      locator<ConfigurationService>().setValues(listeningPort: server.port);
+      server.listen((request) {
+        locator<SessionReplayService>().handleMockRequest(request);
+      });
+    });
+
+    // NOTE: perhaps we can listen to session_mate_cli to close the server,
+    // otherwise, is going to be closed when the app is killed.
+  }
 }
