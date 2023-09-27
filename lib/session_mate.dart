7431555c980fe96cf952a5fc6df1f376d84e4eea
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-27 14:34:52 -0300

    chore: expose SessionMateNavigatorObserver class

--- lib/session_mate.dart
+++ lib/session_mate.dart
@@ -5,5 +5,6 @@ export 'src/session_mate_utils.dart';
 export 'src/setup_code.dart';
 export 'src/widgets/driver_ui/driver_ui.dart';
 export 'src/widgets/session_mate.dart';
+export 'src/widgets/session_mate_navigator_observer.dart';
 export 'src/widgets/interaction_recorder/interaction_recorder.dart';
 export 'src/widgets/session_mate_builder/session_mate_builder.dart';
