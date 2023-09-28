8e7e077f1921278ffb581763e49b0dd91d7b7f1a
Fernando Ferrara <ferrarafer@gmail.com>
2023-09-27 22:43:41 -0300

    feat: update Bookshelf to pass exception and stackTrace

--- example/lib/ui/views/book_details/book_details_viewmodel.dart
+++ example/lib/ui/views/book_details/book_details_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:bookshelf/app/app.locator.dart';
+import 'package:bookshelf/app/app.logger.dart';
 import 'package:bookshelf/models/book.dart';
 import 'package:session_mate/session_mate.dart';
 import 'package:stacked/stacked.dart';
@@ -8,25 +9,32 @@ class BookDetailsViewModel extends FormViewModel {
   final Book book;
   BookDetailsViewModel({required this.book});
 
+  final _logger = getLogger('BookDetailsViewModel');
   final _dialogService = locator<DialogService>();
 
   Future<void> orderBook() async {
-    // if (!isFormValid) {
-    //   _dialogService.showDialog(
-    //     title: 'Form Validation',
-    //     description: inscriptionValidationMessage,
-    //   );
-    //   return;
-    // }
+    try {
+      // if (!isFormValid) {
+      //   _dialogService.showDialog(
+      //     title: 'Form Validation',
+      //     description: inscriptionValidationMessage,
+      //   );
+      //   return;
+      // }
+      if (true) {
+        _dialogService.showDialog(
+          title: 'Unexpected error',
+          description:
+              'We could not place an order for this book. Please contact support with the following error.\n\nErr 205 - id not found in database',
+        );
+      }
 
-    if (true) {
-      _dialogService.showDialog(
-        title: 'Unexpected error',
-        description:
-            'We could not place an order for this book. Please contact support with the following error.\n\nErr 205 - id not found in database',
+      throw Exception(
+        'We could not place an order for this book. Please contact support with the following error.\n\nErr 205 - id not found in database',
       );
+    } catch (e, s) {
+      _logger.e('$e');
+      SessionMateUtils.saveSession(exception: e, stackTrace: s);
     }
-
-    SessionMateUtils.saveSession();
   }
 }
