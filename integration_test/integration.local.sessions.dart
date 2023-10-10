52cf115b42c3240dda30ba11b6163181556ff78f
Dane Mackier <dane.mackier@outlook.com>
2023-10-10 08:38:30 +0200

    Addes scrollables to the integration test

--- integration_test/integration.local.sessions.dart
+++ integration_test/integration.local.sessions.dart
@@ -17,14 +17,21 @@ void registerLocalSessionTests() {
         createdAtTimestamp: DateTime.now().millisecondsSinceEpoch,
         events: [
           TapEvent(
-            position: EventPosition(
-                capturedDeviceHeight: 10,
-                capturedDeviceWidth: 10,
-                x: 10,
-                xDeviation: 10,
-                y: 10,
-                yDeviation: 10),
-          )
+              position: EventPosition(
+                  capturedDeviceHeight: 10,
+                  capturedDeviceWidth: 10,
+                  x: 10,
+                  xDeviation: 10,
+                  y: 10,
+                  yDeviation: 10),
+              externalities: [
+                ScrollableDescription(
+                  axis: ScrollAxis.horizontal,
+                  rect: ScrollableRect(0, 0, 0, 0),
+                  scrollOffsetInPixels: 0,
+                  maxScrollExtentByPixels: 0,
+                )
+              ])
         ],
         id: '1234',
         sessionStats: SessionStats(occurrences: 1),
