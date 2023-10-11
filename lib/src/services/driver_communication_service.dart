dedc0f3567133e3fdd2ed02211e61106601b4a2b
Dane Mackier <dane.mackier@outlook.com>
2023-10-11 13:11:17 +0200

    Resets navigationStack after a replay

--- lib/src/services/driver_communication_service.dart
+++ lib/src/services/driver_communication_service.dart
@@ -2,12 +2,16 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:session_mate/src/app/locator_setup.dart';
+import 'package:session_mate/src/services/session_service.dart';
 import 'package:session_mate_core/session_mate_core.dart';
 import 'package:stacked/stacked.dart';
 
 enum _DriverCommunicationState { waitForReplay, replayActive, freshStart }
 
 class DriverCommunicationService with ListenableServiceMixin {
+  final _sessionService = locator<SessionService>();
+
   Completer<String>? _communicationCompleter;
 
   _DriverCommunicationState _state = _DriverCommunicationState.freshStart;
@@ -30,6 +34,7 @@ class DriverCommunicationService with ListenableServiceMixin {
     if (_wasReplayExecuted) _onReplayCompletedCallback?.call();
 
     _state = _DriverCommunicationState.waitForReplay;
+    _sessionService.clearNavigationStack();
     notifyListeners();
 
     return _communicationCompleter!.future;
