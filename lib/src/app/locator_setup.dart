f42eed4ec5cc2f81c8bcdb722b8bca8e13ab01ed
Dane Mackier <dane.mackier@outlook.com>
2023-10-20 08:58:45 +0200

    Removes old input recording functionality from the interaction recorder

--- lib/src/app/locator_setup.dart
+++ lib/src/app/locator_setup.dart
@@ -12,6 +12,7 @@ import 'package:session_mate/src/services/session_service.dart';
 import 'package:session_mate/src/utils/notification_extractor.dart';
 import 'package:session_mate/src/utils/reactive_scrollable.dart';
 import 'package:session_mate/src/utils/scroll_applicator.dart';
+import 'package:session_mate/src/utils/text_input_recorder.dart';
 import 'package:session_mate/src/utils/time_utils.dart';
 import 'package:session_mate/src/utils/widget_finder.dart';
 import 'package:session_mate/src/widgets/session_mate_route_tracker.dart';
@@ -33,6 +34,7 @@ Future<void> setupLocator() async {
   locator.registerLazySingleton(() => NotificationExtractor());
   locator.registerLazySingleton(() => ReactiveScrollable());
   locator.registerLazySingleton(() => ScrollApplicator());
+  locator.registerLazySingleton(() => TextInputRecorder());
 
   final hiveStorage = HiveService();
   await hiveStorage.init();
