c7ba626c84194d1f0f9c54984b2a8e371ad88f97
Dane Mackier <dane.mackier@outlook.com>
2023-10-20 08:32:24 +0200

    Adds input tracking diff check to know when users entered text

--- lib/src/utils/text_input_recorder.dart
+++ lib/src/utils/text_input_recorder.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:session_mate/src/app/locator_setup.dart';
+import 'package:session_mate/src/app/logger.dart';
 import 'package:session_mate/src/utils/widget_finder.dart';
 import 'package:session_mate_core/session_mate_core.dart';
 
 class TextInputRecorder {
+  final log = getLogger('TextInputRecorder');
   final _widgetFinder = locator<WidgetFinder>();
 
-  final List<_TrackedTextInputItem> _textFieldsOnScreen = [];
+  final Map<Rect, _TrackedTextInputItem> _textFieldsOnScreen = {};
 
   Future<void> populateCurrentTextInfo() async {
     final textFieldInformation = _widgetFinder.getAllTextFieldsOnScreen();
@@ -18,16 +20,42 @@ class TextInputRecorder {
         value: textEditingController.text,
       );
 
-      _textFieldsOnScreen.add(trackedInputItem);
+      _textFieldsOnScreen[boundingBox] = trackedInputItem;
     }
   }
 
   List<InputEvent> checkForTextChange() {
-    // 1. Get all textFields on screen
-    // 2. Compare the start value with the latest textField controller
-    // 3. If there's a difference add the new value as an input event
-    // at the center of the bounding box
-    return [];
+    final List<InputEvent> textInputEvents = [];
+    final textFieldInformation = _widgetFinder.getAllTextFieldsOnScreen();
+
+    for (var (textEditingController, boundingBox) in textFieldInformation) {
+      final trackedTextFieldItem = _textFieldsOnScreen[boundingBox];
+
+      if (trackedTextFieldItem == null) {
+        log.e(
+            '''SESSION MATE ERROR: No text field is found at the current position $boundingBox
+
+        This means the textField was changed without recalculating the position. Please contact
+        dane@sessionmate.dev with this issue, this should never happen.
+        ''');
+      } else {
+        final inputHasChanged =
+            trackedTextFieldItem.value != textEditingController.text;
+
+        if (inputHasChanged) {
+          final textInputPosition = boundingBox.center;
+
+          textInputEvents.add(InputEvent(
+            position: EventPosition(
+              x: textInputPosition.dx,
+              y: textInputPosition.dy,
+            ),
+          ));
+        }
+      }
+    }
+
+    return textInputEvents;
   }
 
   void clearTextInfo() {
