b881692f24a91a2962c88af9ea8c39d958331d8f
Dane Mackier <dane.mackier@outlook.com>
2023-10-22 15:34:44 +0200

    - Adds circular progress indicator to login view - Adds login button to login view

--- example/lib/ui/views/login/login_view.dart
+++ example/lib/ui/views/login/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookshelf/ui/common/ui_helpers.dart';
 import 'package:bookshelf/ui/views/login/login_view.form.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
@@ -35,6 +36,29 @@ class LoginView extends StackedView<LoginViewModel> with $LoginView {
               controller: passwordController,
               textInputAction: TextInputAction.done,
               onFieldSubmitted: (_) => viewModel.login(),
+            ),
+            const SizedBox(height: 30),
+            if (viewModel.isBusy)
+              const CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            verticalSpaceLarge,
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                onPressed: viewModel.login,
+                style: FilledButton.styleFrom(
+                  backgroundColor: const Color(0xFF514DC3),
+                  foregroundColor: const Color(0xFFFFFFFF),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
             ),
           ],
         ),
