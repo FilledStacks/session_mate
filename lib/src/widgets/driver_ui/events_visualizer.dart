cdd55b398060fe93db2c9cd7be4044933820501b
Fernando Ferrara <ferrarafer@gmail.com>
2023-10-23 23:26:50 -0300

    feat: remove filters on the UIEvents at screen

--- lib/src/widgets/driver_ui/events_visualizer.dart
+++ lib/src/widgets/driver_ui/events_visualizer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:session_mate/src/extensions/event_position_extensions.dart';
-import 'package:session_mate/src/utils/interaction_utils.dart';
 import 'package:session_mate/src/widgets/driver_ui/common/event_visual.dart';
 import 'package:session_mate/src/widgets/driver_ui/driver_ui_viewmodel.dart';
 import 'package:session_mate_core/session_mate_core.dart';
@@ -17,24 +16,17 @@ class EventsVisualizer extends ViewModelWidget<DriverUIViewModel> {
         builder: (_, events, __) {
           return Stack(
             children: [
-              ...events
-                  .where(
-                      (event) => InteractionUtils.visibleOnScreen(event, size))
-                  .where((event) =>
-                      event.view == viewModel.currentView &&
-                      event.navigationStackId ==
-                          viewModel.currentNavigationStackId)
-                  .map(
-                    (event) => Positioned(
-                      key: Key(event.automationKey),
-                      top: event.position.responsiveYPosition(size.height),
-                      left: event.position.responsiveXPosition(size.width),
-                      child: EventVisual(
-                        event: event,
-                        index: events.indexOf(event),
-                      ),
-                    ),
-                  )
+              ...events.map(
+                (event) => Positioned(
+                  key: Key(event.automationKey),
+                  top: event.position.responsiveYPosition(size.height),
+                  left: event.position.responsiveXPosition(size.width),
+                  child: EventVisual(
+                    event: event,
+                    index: events.indexOf(event),
+                  ),
+                ),
+              )
             ],
           );
         });
