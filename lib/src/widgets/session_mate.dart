00390328add8603a9a890115e44f0d507c6ea630
Fernando Ferrara <ferrarafer@gmail.com>
2023-10-24 15:29:38 -0300

    feat: allow SessionMate to be disable

--- lib/src/widgets/session_mate.dart
+++ lib/src/widgets/session_mate.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:session_mate/src/app/locator_setup.dart';
+import 'package:session_mate/src/services/configuration_service.dart';
 
 class SessionMate extends StatefulWidget {
   final Widget child;
@@ -23,6 +25,8 @@ class _SessionMateState extends State<SessionMate> {
 
   @override
   Widget build(BuildContext context) {
+    if (!locator<ConfigurationService>().enabled) return widget.child;
+
     return KeyedSubtree(key: key, child: widget.child);
   }
 }
