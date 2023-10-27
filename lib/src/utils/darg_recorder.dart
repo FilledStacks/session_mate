e07b301464299394cf52cc283ff09bde565206e5
Dane Mackier <dane.mackier@outlook.com>
2023-10-27 16:38:34 +0200

    Adds the drag recorder functionality

---
+++ lib/src/utils/darg_recorder.dart
@@ -0,0 +1,49 @@
+import 'package:session_mate/src/helpers/logger_helper.dart';
+import 'package:session_mate_core/session_mate_core.dart';
+
+class DragRecorder {
+  DragEvent? _activeDrag;
+  Stopwatch? _timer;
+
+  bool get isRecording => _activeDrag != null;
+
+  void startDragRecording({required EventPosition position}) {
+    logUIEvent('Start drag recording: $position');
+    _activeDrag = DragEvent(
+      position: position,
+    );
+
+    _timer = Stopwatch()..start();
+  }
+
+  void clearDragRecording() {
+    logUIEvent('Clear drag event');
+
+    _activeDrag = null;
+    _timer?.stop();
+    _timer = null;
+  }
+
+  DragEvent completeDragEvent({required EventPosition endPosition}) {
+    if (_activeDrag == null) {
+      logUIEvent(
+        'A drag event was not started, but we\'re trying to complete one',
+      );
+      throw Exception('Drag cannot be completed if it was not started.');
+    }
+
+    final scrollDelta = _activeDrag!.position - endPosition;
+
+    _activeDrag = _activeDrag!.copyWith(
+      scrollDelta: scrollDelta,
+      duration: _timer?.elapsedMilliseconds ?? -1,
+    );
+
+    logUIEvent(
+      'Drag completed in ${_timer?.elapsed.inSeconds} seconds. ',
+      event: _activeDrag!,
+    );
+
+    return _activeDrag!;
+  }
+}
---
+++ test/utils/drag_recorder_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:session_mate/src/app/locator_setup.dart';
+import 'package:session_mate/src/utils/darg_recorder.dart';
+import 'package:session_mate_core/session_mate_core.dart';
+
+import '../helpers/test_helpers.dart';
+
+void main() {
+  group('DragRecorderTest -', () {
+    setUp(() => registerServices());
+    tearDown(() => locator.reset());
+
+    group('Record functionality -', () {
+      test(
+          'When we start a record and complete event, should get event with correct duration and positions',
+          () async {
+        final recorer = DragRecorder();
+
+        recorer.startDragRecording(
+          position: EventPosition(
+            x: 1,
+            y: 1,
+          ),
+        );
+
+        await Future.delayed(const Duration(seconds: 1));
+
+        final result = recorer.completeDragEvent(
+          endPosition: EventPosition(
+            x: 2,
+            y: 2,
+          ),
+        );
+
+        expect(
+          result.scrollDelta.x,
+          -1,
+          reason: 'scroll deltaX is start - end position',
+        );
+
+        expect(
+          result.scrollDelta.y,
+          -1,
+          reason: 'scroll deltaY is start - end position',
+        );
+
+        expect(result.duration > 1000, true);
+      });
+
+      test('When we start a record and cancel, isRecording should be false',
+          () async {
+        final recorer = DragRecorder();
+
+        recorer.startDragRecording(
+          position: EventPosition(
+            x: 1,
+            y: 1,
+          ),
+        );
+
+        recorer.clearDragRecording();
+
+        expect(recorer.isRecording, false);
+      });
+    });
+  });
+}
