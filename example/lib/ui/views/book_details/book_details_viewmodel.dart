d17be0d0cdf7cee529647bc9eb2a01f8d259ed2a
Fernando Ferrara <ferrarafer@gmail.com>
2023-11-20 12:30:23 -0300

    fix: update SessionList to show proper messages

--- example/lib/ui/views/book_details/book_details_viewmodel.dart
+++ example/lib/ui/views/book_details/book_details_viewmodel.dart
@@ -1,5 +1,4 @@
 import 'package:bookshelf/app/app.locator.dart';
-import 'package:bookshelf/app/app.logger.dart';
 import 'package:bookshelf/models/book.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -8,7 +7,6 @@ class BookDetailsViewModel extends FormViewModel {
   final Book book;
   BookDetailsViewModel({required this.book});
 
-  final _logger = getLogger('BookDetailsViewModel');
   final _dialogService = locator<DialogService>();
 
   double _quantity = 1;
